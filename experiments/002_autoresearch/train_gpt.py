386495830e43a5a23a2245a77fb137092836a8a6
abaybektursun <abaybektursun@users.noreply.github.com>
2026-03-20 20:42:32 +0000

    exp: retune warmdown for sdpa pr198 shape

--- experiments/002_autoresearch/train_gpt.py
+++ experiments/002_autoresearch/train_gpt.py
@@ -56,7 +56,7 @@ class Hyperparameters:
 
     # Training length.
     iterations = int(os.environ.get("ITERATIONS", 9000))
-    warmdown_iters = int(os.environ.get("WARMDOWN_ITERS", 3000))
+    warmdown_iters = int(os.environ.get("WARMDOWN_ITERS", 2500))
     warmup_steps = int(os.environ.get("WARMUP_STEPS", 20))
     train_batch_tokens = int(os.environ.get("TRAIN_BATCH_TOKENS", 786_432))
     train_seq_len = int(os.environ.get("TRAIN_SEQ_LEN", 2048))
